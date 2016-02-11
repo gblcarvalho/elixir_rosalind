869b48330d95dc283f8618122080f67f3e352627
gblmiranda <gabrielm.car@gmail.com>
2016-02-11 21:42:33 -0200

    Answer PROT

--- rosalind.ex
+++ rosalind.ex
@@ -21,13 +21,17 @@ defmodule Rosalind do
     [n, k] = for i <- String.split(args), do: String.to_integer(i)
     DynamicProgramming.rabbits_recurrence_relations(n, k) |> Integer.to_string
   end
-  
+
   def gc(args) do
     {max_label, max_gc} = args
                           |> FASTA.group
                           |> DNA.highest_gc_content
-    
+
     "#{max_label}\n#{Float.to_string(max_gc, [compact: true])}"
+  end
+
+  def prot(rna) do
+    DNA.encoding_rna_into_aminoacid(rna)
   end
 end
 
@@ -46,17 +50,49 @@ defmodule DNA do
     complement = for n <- String.codepoints(dna), into: "", do: complements[n]
     String.reverse(complement)
   end
-  
+
   def highest_gc_content(dnas) do
     (for {label, dna} <- dnas, do: {label, gc_content(dna)})
     |> Enum.max_by(fn(gc) -> elem(gc, 1) end)
   end
-  
+
   def gc_content(dna) do
     counting_chars(dna, ["G","C"])
     |> Map.values
     |> Enum.reduce(0, &(&1 + &2))
     |> (fn(gc) -> (gc * 100)/ String.length(dna) end).()
+  end
+
+  def encoding_rna_into_aminoacid(rna) do
+    rna_codon_table = %{"UUU" => "F", "CUU" => "L", "AUU" => "I", "GUU" => "V",
+                        "UUC" => "F", "CUC" => "L", "AUC" => "I", "GUC" => "V",
+                        "UUA" => "L", "CUA" => "L", "AUA" => "I", "GUA" => "V",
+                        "UUG" => "L", "CUG" => "L", "AUG" => "M", "GUG" => "V",
+                        "UCU" => "S", "CCU" => "P", "ACU" => "T", "GCU" => "A",
+                        "UCC" => "S", "CCC" => "P", "ACC" => "T", "GCC" => "A",
+                        "UCA" => "S", "CCA" => "P", "ACA" => "T", "GCA" => "A",
+                        "UCG" => "S", "CCG" => "P", "ACG" => "T", "GCG" => "A",
+                        "UAU" => "Y", "CAU" => "H", "AAU" => "N", "GAU" => "D",
+                        "UAC" => "Y", "CAC" => "H", "AAC" => "N", "GAC" => "D",
+                        "UAA" => "Stop", "CAA" => "Q", "AAA" => "K", "GAA" => "E",
+                        "UAG" => "Stop", "CAG" => "Q", "AAG" => "K", "GAG" => "E",
+                        "UGU" => "C", "CGU" => "R", "AGU" => "S", "GGU" => "G",
+                        "UGC" => "C", "CGC" => "R", "AGC" => "S", "GGC" => "G",
+                        "UGA" => "Stop", "CGA" => "R", "AGA" => "R", "GGA" => "G",
+                        "UGG" => "W", "CGG" => "R", "AGG" => "R", "GGG" => "G"}
+
+    do_encoding_rna_into_aminoacid(rna, rna_codon_table, "")
+  end
+
+  defp do_encoding_rna_into_aminoacid("", _table, protein), do: protein
+  defp do_encoding_rna_into_aminoacid(rna, table, protein) do
+    case table[String.slice(rna, 0, 3)] do
+      "Stop" -> protein
+      nil -> do_encoding_rna_into_aminoacid(String.slice(rna, 1..-1),
+                                            table,
+                                            protein <> String.slice(rna, 1, 1))
+      p -> do_encoding_rna_into_aminoacid(String.slice(rna, 3..-1), table, protein <> p)
+    end
   end
 
   defp counting_chars(string, chars) do
@@ -68,6 +104,7 @@ defmodule DNA do
       end
     end)
   end
+
 end
 
 
@@ -107,11 +144,12 @@ defmodule FASTA do
 end
 
 
+
 {:ok, file} = File.open "result.txt", [:write]
 
-case File.read("rosalind_gc.txt") do
+case File.read("rosalind_prot.txt") do
 #case File.read("test.txt") do
-  {:ok, body}      -> IO.binwrite file, (body |> String.strip |> Rosalind.gc)
+  {:ok, body}      -> IO.binwrite file, (body |> String.strip |> Rosalind.prot)
   {:error, reason} -> IO.puts "Error when open the file: #{reason}"
 end
 
